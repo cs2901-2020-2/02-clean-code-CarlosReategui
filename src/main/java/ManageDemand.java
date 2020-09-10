6281c5d1bf8d5d1e86d2c2469f0a84fcc66a1c3c
CarlosReategui <carlos.reategui@utec.edu.pe>
2020-09-10 08:33:38 -0500

    filledOrders completed

--- src/main/java/ManageDemand.java
+++ src/main/java/ManageDemand.java
@@ -1,8 +1,19 @@
 import java.util.List;
+import java.util.logging.Logger;
 
 public class ManageDemand {
-    public long filledOrders(List<Long> orders, long k){
+    public static long filledOrders(List<Long> orders, long k){
         //TODO add your code here
-        return 0;
+        int totalWidgets = 0;
+        int n = orders.size();
+        int totalOrders = 0;
+        while (totalOrders < n) {
+            totalWidgets += orders.get(totalOrders);
+            if (totalWidgets > k) {
+                break;
+            }
+            totalOrders += 1;
+        }
+        return totalOrders;
     }
 }
